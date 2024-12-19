4f09752312b4d27f00b15ef07f84fa906201a6e0
Liudas Dzisevicius <liudas.dzisevicius@gmail.com>
2024-12-19 13:26:02 +0200

    Fix textarea id

--- src/TextArea.jsx
+++ src/TextArea.jsx
@@ -1,10 +1,10 @@
 import Label from './Label';
 
-export default function TextArea({ label, onChange, value, error }) {
+export default function TextArea({ label, id, onChange, value, error }) {
     return (
         <div className="flex flex-col gap-2">
-            <Label id="" label={label} />
-            <textarea onChange={onChange} value={value} className={`w-full px-5 py-2 text-gray-900 border border-gray-500 rounded-md hover:border-green-600 focus-visible:outline-none focus-visible:border focus-visible:border-green-600 ${error?'border-red':''}`} rows={3} name="" id=""></textarea>
+            <Label id={id} label={label} />
+            <textarea onChange={onChange} value={value} id={id} className={`w-full px-5 py-2 text-gray-900 border border-gray-500 rounded-md hover:border-green-600 focus-visible:outline-none focus-visible:border focus-visible:border-green-600 ${error?'border-red':''}`} rows={3}></textarea>
             {error ?
                 <span className="text-red">{error}</span>
             :''}
